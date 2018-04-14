288bfeb3f3bece4d43a860b64b313c90e2e9db3a
Georgiy <deezchannel@gmail.com>
2018-04-14 12:09:59 +0300

    Add scripts

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# aws_install
+# aws_jupyter_anaconda
+Script for installing Miniconda, Jupyter Notebook, VowpalWabbit in AWS instance
+
+ - Specify your AWS key-pair pemfile, AWS host
+ - run launch_remote_setup.sh PEMFILE_PATH HOST_IP
+ - go to https://YOUR_AWS_HOST:8888 and have fun!
+
+After end of work run *EC2 -> Instances -> Actions -> Instance State -> Terminate*
+ 
---
+++ remote_setup.sh
@@ -0,0 +1,34 @@
+# Installing Miniconda
+wget -c http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh
+bash Miniconda-latest-Linux-x86_64.sh -b -p /home/ubuntu/miniconda
+export PATH=/home/ubuntu/miniconda/bin:$PATH
+
+#Installing neccesary libraries
+conda install -y numpy scipy pandas scikit-learn jupyter tqdm matplotlib seaborn
+conda install -c conda-forge jupyter_contrib_nbextensions
+
+#Can add whatever you want to install
+# sudo apt-get -qq install vowpal-wabbit
+sudo apt-get update
+sudo apt-get install build-essential
+sudo apt-get install libboost-program-options-dev zlib1g-dev
+sudo apt-get install default-jdk
+sudo git clone git://github.com/JohnLangford/vowpal_wabbit.git
+cd vowpal_wabbit
+sudo CPLUS_INCLUDE_PATH='/usr/lib/jvm/java-8-openjdk-amd64/include:/usr/lib/jvm/java-8-openjdk-amd64/include/linux' make install
+
+sudo apt-get -qq install git
+
+wget "https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA" -O gdrive
+chmod +x gdrive-linux-x64
+
+ipython profile create nbserver
+
+printf "\n# Configuration file for ipython-notebook.\n
+c = get_config()\n
+# Notebook config\n
+c.NotebookApp.ip = '*'\n
+c.NotebookApp.open_browser = False\n
+c.NotebookApp.port = 8888\n" > ~/.ipython/profile_nbserver/ipython_notebook_config.py
+
+jupyter-notebook --config="~/.ipython/profile_nbserver/ipython_notebook_config.py"
